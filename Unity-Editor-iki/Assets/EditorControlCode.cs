bd2322f74db7f24dcc2f4e5ca0699f357ed00f08
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2020-07-08 15:40:44 +0300

    8.07.2020
    istediğimi yaptım güzel bir ray elde ettim bunla objelerimi tutup dilediğim gibi oynatabilirim

---
+++ Unity-Editor-iki/Assets/EditorControlCode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EditorControlCode : MonoBehaviour
+{
+    /* 
+     * eğer hareket et butonuna basılmışsa
+     * mousemun olduğu konuma bak
+     *  orada obje varsa onu seç ve update içerisinde o nesneye  mousenin konumunu eşitle
+     *  objeye tekrar dokununca bırak
+     *
+     */
+    /*başlangıç konumunu al Raybaslangıc
+     * bitiş konumunu al ınput.mouse
+     * bu ikisi arasında ray çiz ve raycasthit al
+     * raycasthit in adını yazdır
+     */
+    public Collider2D baslangic;
+    Physics2DRaycaster dRaycaster;
+    RaycastHit2D raycastHit;
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycast;
+        Physics.Raycast(ray.origin, ray.direction*200,out raycast);
+        Debug.DrawRay(ray.origin, ray.direction*20,Color.red,3,false);
+        Debug.Log(raycastHit.collider.name);
+    }
+    private void OnMouseOver()
+    {
+        print("deneme");
+    }
+}
